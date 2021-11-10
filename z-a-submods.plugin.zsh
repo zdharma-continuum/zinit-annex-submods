5cde0f9ff81bd67e694f649edb82867c972e8972
Philipp Schmitt <philipp@schmitt.co>
2021-11-10 18:27:37 +0100

    Update url

--- z-a-submods.plugin.zsh
+++ z-a-submods.plugin.zsh
@@ -1,5 +1,5 @@
 # Standarized $0 handling, see:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#zero-handling
 
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
