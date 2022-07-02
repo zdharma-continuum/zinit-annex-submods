b87081a0f2bb23627c023a2b5b38f981fa9a1c36
Vladislav Doster <mvdoster@gmail.com>
2022-07-02 12:08:53 -0500

    fix: simplify whitespace trimming pattern
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- z-a-submods.plugin.zsh
+++ z-a-submods.plugin.zsh
@@ -1,5 +1,4 @@
 #!/usr/bin/env zsh
-# -*- mode: sh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
 #
 # Copyright (c) 2016-2020 Sebastian Gniazdowski and contributors
 # Copyright (c) 2021-2022 zdharma-continuum and contributors
